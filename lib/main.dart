9a2d2850822ae2b9fb6292eae8f8d7ec5347eddf
ahmad-kalaf <ahmad.it@outlook.de>
2025-08-01 20:39:05 +0200

    HomePage bearbeitet und Konstanten für Farben und Schriftgrößen hinzugefügt

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,73 @@
 import 'package:flutter/material.dart';
+import 'package:praxisassistent/utils/constants.dart';
 
 void main() {
+  // Start der Anwendung
   runApp(const PraxisassistentApp());
 }
 
+// Hauptanwendungsklasse
 class PraxisassistentApp extends StatelessWidget {
   const PraxisassistentApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // MaterialApp-Widget, umschließt die gesamte App
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Praxisassistent',
-      home: const MyHomePage(title: 'Startbildschirm'),
+      home: const HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+class HomePage extends StatefulWidget {
+  // Konstruktor der Homepage
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Placeholder(child: Center(child: Text("Platzhalter")));
+    return Scaffold(
+      // AppBar mit Titel und Hintergrundfarbe
+      appBar: AppBar(
+        title: const Text('Praxisassistent'),
+        centerTitle: true,
+        backgroundColor: kAppBarBackgroundColor,
+        titleTextStyle: TextStyle(
+          fontSize: kDefaultAppBarFontSize,
+          color: kDefaultTextColor,
+        ),
+      ),
+      // Container mit Gradient-Hintergrundfarbe (Farbverlauf)
+      // Dieser Container umschließt die weiteren Widgets im Body der HomePage
+      body: Container(
+        decoration: BoxDecoration(
+          // Gradient-Hintergrundfarbe (Linearer Farbverlauf von unten links nach oben rechts)
+          gradient: LinearGradient(
+            colors: [kScaffoldGradientStartColor, kScaffoldGradientEndColor],
+            begin: Alignment.bottomLeft,
+            end: Alignment.topRight,
+          ),
+        ),
+        child: SafeArea(
+          // Sicherer Bereich, um Inhalte nicht hinter der Stautsleiste oder
+          // anderen UI-Elementen des Betriebssystems zu verstecken
+          child: Center(
+            child: Text(
+              'Willkommen im Praxisassistent',
+              style: TextStyle(
+                fontSize: kDefaultBodyFontSize,
+                color: kDefaultTextColor,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
---
+++ lib/utils/constants.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+// Farben
+final Color kAppBarBackgroundColor = Colors.grey[600]!;
+final Color kScaffoldGradientStartColor = Colors.grey[800]!;
+final Color kScaffoldGradientEndColor = Colors.grey[400]!;
+final Color kDefaultTextColor = Colors.white;
+
+// Schriftgrößen
+final double kDefaultAppBarFontSize = 24.0;
+final double kDefaultBodyFontSize = 18.0;
