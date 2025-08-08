45e964a240f66213e3e5ef2758013c7229a1816a
ahmad-kalaf <ahmad.it@outlook.de>
2025-08-08 11:25:41 +0200

    Platzhalter für RegistrationWidget erstellt

---
+++ lib/widgets/registration_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class RegistrationWidget extends StatefulWidget {
+  const RegistrationWidget({super.key});
+
+  @override
+  State<RegistrationWidget> createState() => _RegistrationWidgetState();
+}
+
+class _RegistrationWidgetState extends State<RegistrationWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
